946a912d6b2976379580e9a0312087df7fd23525
马军 <majun@xiwicloud.com>
2024-12-16 09:15:49 +0800

    ✨ feat:带参数请求

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,8 +1,26 @@
-import { Controller,Get } from '@nestjs/common';
+import { Controller, Get, Req, Query, Param, Post, Body } from '@nestjs/common';
 
 @Controller('a')
 export class AppController {
   constructor() {}
+  @Get(':id')
+  findAll(@Param() query: any): string {
+    console.log(query, '444');
+
+    return 'This action returns all cats';
+  }
+
+  @Post('create')
+  createCat(@Body() body: any): string {
+    console.log(body, '555');
+    return `Created cat with data: ${JSON.stringify(body)}`;
+  }
+
+  // findAll(@Query() request: Request): string {
+  //   console.log(request, '444');
+
+  //   return 'This action returns all cats';
+  // }
 
   @Get('b')
   getHello(): string {
